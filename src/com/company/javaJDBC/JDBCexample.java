ad5ebc76aa7027a721a56ffae659e11a89aab1fe
Rupesh Bhatta <rupesh.bhatta1234@gmail.com>
2023-05-07 08:25:24 +0545

    done

--- src/com/company/javaJDBC/JDBCexample.java
+++ src/com/company/javaJDBC/JDBCexample.java
@@ -3,7 +3,7 @@ package com.company.javaJDBC;
 import java.sql.*;
 
 public class JDBCexample {
-    public static void main(Strings args[]) throws SQLException{
+    public static void main(String args[]) throws SQLException{
         try{
             // load or register drivers
             Class.forName("com.mysql.jdbc.Driver");
@@ -15,9 +15,8 @@ public class JDBCexample {
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery("select * from students");
             while (rs.next()) {
-
+                System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3) + " " + rs.getInt(4));
             }
-
         }
         catch(Exception e){
             System.out.println(e);
